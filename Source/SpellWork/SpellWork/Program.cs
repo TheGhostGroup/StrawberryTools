329656194c5d176330731b51d1323757860dbb92
Mark <prelude_20032002@yahoo.com>
2012-05-24 15:47:36 -0400

    Added Spellwork Source and updated it to 4.3.4 dbc

---
+++ Source/SpellWork/SpellWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SpellWork
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("./dbc"))
+            {
+                MessageBox.Show("Could not find DBC files, please place them in a folder called dbc within the executable directory");
+                return;
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            new Loader();
+            Application.Run(new FormMain());
+        }
+    }
+}
